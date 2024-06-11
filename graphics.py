860df3f8809fa710fcdd039d4570bde1dbf1bcb4
bepip <beri073@hotmail.fr>
2024-06-11 15:50:42 +0200

    maze class added

--- graphics.py
+++ graphics.py
@@ -1,22 +1,5 @@
 from tkinter import Tk, BOTH, Canvas
 
-class Point():
-    def __init__(self,x,y):
-        self.x = x
-        self.y = y
-
-class Line():
-    def __init__(self, p1, p2):
-        self.p1 = p1
-        self.p2 = p2
-
-    def draw_line(self, canvas, fill_color):
-        x1 = self.p1.x
-        x2 = self.p2.x
-        y1 = self.p1.y
-        y2 = self.p2.y
-        canvas.create_line(x1, y1, x2, y2, fill = fill_color, width = 2)
-
 class Window():
     def __init__(self, width, height):
         self.__root = Tk()
@@ -40,3 +23,24 @@ class Window():
 
     def draw_line(self, line, fill_color = "black"):
         line.draw_line(self.__canvas, fill_color)
+
+class Point():
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+class Line():
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw_line(self, canvas, fill_color):
+        x1 = self.p1.x
+        x2 = self.p2.x
+        y1 = self.p1.y
+        y2 = self.p2.y
+        canvas.create_line(x1, y1, x2, y2, fill = fill_color, width = 2)
+
+
+
+
--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-from window import (
-    Window, 
-    Line,
-    Point
+from graphics import (
+    Window,
+    Point,
+    Line
 )
+from cell import Cell
+from maze import Maze
 
 def main():
-    height = 600
-    width = 800
-    win = Window(width, height)
-    line = Line(Point(0,0), Point(width, height))
-    win.draw_line(line)
-    win.wait_for_close()
+    num_rows = 12
+    num_cols = 16
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
+    win = Window(screen_x, screen_y)
 
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
+
+    win.wait_for_close()
 main()
